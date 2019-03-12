bc3b9c42fbdf4f769e1233ade7c15adb08094252
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-03-12 14:44:47 -0400

    Add a generic home route

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const server = Hapi.server({
   host: "localhost"
 });
 
+// Routes
+server.route({
+  method: "GET",
+  path: "/",
+  handler: (request, h) => {
+    return "Hello World!";
+  }
+});
+
 const init = async () => {
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
