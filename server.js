d94eb4764fbc3eef01f9130327920b18c8c04fcf
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-03-13 12:09:14 -0400

    added some more route functionality

--- server.js
+++ server.js
@@ -8,11 +8,38 @@ const server = Hapi.server({
 });
 
 // Routes
+server.route([
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Hello World!";
+    },
+    // the config option doesnt effect the routes functionality, but is helpful and valuable when generating the apps documentation or bringing new developers on board.
+    config: {
+      description: "Sends a friendly greeting!",
+      notes: "No route parameters available",
+      tags: ["greeting"]
+    }
+  },
+  {
+    method: "POST",
+    path: "/",
+    handler: (request, h) => {
+      // Process the request's payload
+      return "Created a new instance";
+    }
+  }
+]);
+
 server.route({
   method: "GET",
-  path: "/",
+  path: "/members/{name?}", // Optional parameters - add a ? to the end - only the last element can be optional
   handler: (request, h) => {
-    return "Hello World!";
+    // we URI encode the name parameter to prevent injection attacks
+    return encodeURIComponent(request.params.name)
+      ? `Hello ${encodeURIComponent(request.params.name)}`
+      : "Hello Stranger";
   }
 });
 
